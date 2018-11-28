73c30641840a99eaf16938b6443271fbcbe5c3e9
Chrismarcel <cmarcelj@gmail.com>
2018-11-28 01:03:21 +0100

    [chore #162250388] Add a simple mocha test suite

---
+++ server/test/app.spec.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+describe("test", () => {
+  it("should return a string", () => {
+    expect("ci with travis").to.equal("ci with travis");
+  });
+});
+
+export default function hello() {
+  return "hello";
+}
