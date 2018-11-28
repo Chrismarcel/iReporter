10b3351ac71dcebb180618232f056a7c90d9a6dd
Chrismarcel <cmarcelj@gmail.com>
2018-11-28 01:37:24 +0100

    [chore #162250388] Set up ESLint for code linting

--- server/test/app.spec.js
+++ server/test/app.spec.js
@@ -1,11 +1,11 @@
-import { expect } from "chai";
+import { expect } from 'chai';
 
-describe("test", () => {
-  it("should return a string", () => {
-    expect("ci with travis").to.equal("ci with travis");
+describe('test', () => {
+  it('should return a string', () => {
+    expect('ci with travis').to.equal('ci with travis');
   });
 });
 
 export default function hello() {
-  return "hello";
+  return 'hello';
 }
