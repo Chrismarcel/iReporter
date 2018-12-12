2a383885ea4357c85177b1aa2f41ae1318ec83a2
Chrismarcel <cmarcelj@gmail.com>
2018-12-12 10:33:14 +0100

    chore: refactor db code

--- server/models/index.js
+++ server/models/index.js
@@ -5,6 +5,5 @@ import createQuery from './createTables';
 const queries = `${dropQuery}${createQuery}`;
 
 pool.query(queries, (err, res) => {
-    console.log(err, res);
-    pool.end();
+  pool.end();
 });
