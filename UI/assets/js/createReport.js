b2aab775d4966a275cf3ea82fcdb610263b311e9
Chrismarcel <cmarcelj@gmail.com>
2019-01-23 14:00:04 +0100

    feat: Implement edit report functionality
    - Ensure that authenticated users can edit their reports
    - Ensure that changes made the reports on the front end gets persisted to the database
    
    [(Finishes | Delivers) #163412335]

--- UI/assets/js/createReport.js
+++ UI/assets/js/createReport.js
@@ -1,3 +1,26 @@
+function createReport(body, endpoint, method) {
+  const url = `http://localhost:3000/api/v1/${endpoint}`;
+  const token = window.localStorage.getItem('token');
+
+  fetch(url, {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.json())
+    .then((responseObj) => {
+      console.log(responseObj);
+      if (method !== 'GET') {
+        window.location.href = './view-reports.html';
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+
 document.querySelector('.form-card').addEventListener('submit', (evt) => {
   evt.preventDefault();
   const comment = document.getElementById('comment').value;
@@ -5,11 +28,10 @@ document.querySelector('.form-card').addEventListener('submit', (evt) => {
   const location = document.getElementById('location').value.split(',');
   const latitude = location[0].trim();
   const longitude = location[1].trim();
-  const token = window.localStorage.getItem('token');
 
   const reportObj = {
     latitude, longitude, comment,
   };
 
-  handleReports(reportObj, reportType, 'POST', token);
+  createReport(reportObj, reportType, 'POST');
 });
---
+++ UI/assets/js/editReport.js
@@ -0,0 +1,37 @@
+
+function updateReport(endpoint, type, id, reportObj) {
+  const token = window.localStorage.getItem('token');
+
+  return fetch(`http://localhost:3000/api/v1/${endpoint}/${id}/${type}`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(reportObj),
+  })
+    .then(response => response.json())
+    .then(responseObj => responseObj)
+    .catch(error => console.log(error));
+}
+
+function parseUrl(queryString) {
+  const matches = queryString.substr(1).match(/=(\w+)-?(\w+)?/gm);
+  return matches.map(match => match.substr(1));
+}
+
+document.querySelector('.form-card').addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const comment = document.getElementById('comment').value;
+  const location = document.getElementById('location').value.split(',');
+  const latitude = location[0].trim();
+  const longitude = location[1].trim();
+  const commentObj = { comment };
+  const locationObj = { latitude, longitude };
+  const endpoint = parseUrl(window.location.search);
+
+  Promise.all(
+    [updateReport(endpoint[0], 'location', endpoint[1], locationObj),
+      updateReport(endpoint[0], 'comment', endpoint[1], commentObj)],
+  ).then(response => console.log(response));
+});
